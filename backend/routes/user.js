a08cf7c7cdcba38913e4b856cceee58fc847bb96
Sarah Majewski <sarah.majewski80@gmail.com>
2021-07-09 12:33:31 +0200

    Add all user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,11 +7,21 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 
 // Add middleware
-// For password verification
-const verifyPassword = require('../middleware/password');
+// For password and email verification
+const validUser = require('../middleware/validUser');
+
+// For authentification (secure the roads)
+const auth = require('../middleware/auth');
+
+// For image management
+const multer = require('../middleware/multer-config');
 
 // Creation of the different routes of the API
-router.post('/signup', verifyPassword, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', validUser.valid, userCtrl.signup);
+router.post('/login', validUser.valid, userCtrl.login);
+router.get('/profil', auth, userCtrl.getAllUsers);
+router.put('profil/:id', auth, multer, userCtrl.updateProfil);
+router.get('profil/.id', auth, userCtrl.getProfil);
+router.delete('profil/:id', auth, userCtrl.deleteProfil)
 
 module.exports = router;
