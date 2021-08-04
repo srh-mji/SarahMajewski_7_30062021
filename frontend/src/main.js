dfd44b99037278ddaea92a62610b4f53fd8c6d64
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-04 14:34:53 +0200

    add localstorage.user in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+if (localStorage.user != undefined) {
+  Vue.prototype.$token = JSON.parse(localStorage.user).token;
+  Vue.prototype.$user = JSON.parse(localStorage.user);
+}
+
+// Create Vue instance 
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
