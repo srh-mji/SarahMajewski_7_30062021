d7f956843b527a29ed30610770f5b9c85918fea2
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-04 14:34:25 +0200

    Add path signup, account & post

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../views/Signup.vue'
+import Post from '../views/Post.vue'
+import Account from '../views/Account.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +12,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: Account
+  },
+  {
+    path: '/post',
+    name: 'Post',
+    component: Post
   }
 ]
 
