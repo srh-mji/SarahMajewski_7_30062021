78ff73fb01c4eaad5a70cd9d17e8577a58f6ef4e
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-04 14:32:55 +0200

    Add views

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="home">
+    <Login v-if="!connected" />
+
+    <Header v-if="connected"/>
+    <NewPost v-if="connected"/>
+    <Posts v-if="connected"/>
+  </div>
+</template>
+
+<script>
+import Login from '@/components/Login.vue';
+import Header from '@/components/Header.vue';
+import NewPost from '@/components/NewPost.vue';
+import Posts from '@/components/Posts.vue';
+
+export default({
+    name : 'Home',
+    components : {
+         Login,
+         Header,
+         NewPost,
+         Posts,
+    },
+
+    data() {
+    return{
+      connected: true
+    };
+  },
+
+  created(){
+    this.checkConnected()
+  },
+   methods: {
+    checkConnected(){
+      if(localStorage.user !== undefined){
+        this.connected = true;
+        console.log('Utilisateur connecté !');
+      }
+      else if(localStorage.user == undefined){
+        this.connected = false;
+        console.log('Utilisateur non connecté !');
+      }
+    }},
+})
+</script>
