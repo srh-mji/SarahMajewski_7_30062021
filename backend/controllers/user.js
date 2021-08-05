4cedcce32f201cce9299e6a1a7bc4239b68250b9
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-05 10:49:42 +0200

    User controllers error

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,85 +20,118 @@ const fs = require('fs');
 const passwordSchema = new passwordValidator();
 // Password constraints
 passwordSchema
-.is().min(8)
-.has().uppercase()
-.has().lowercase()
-.has().digits()
-.has().not().spaces()
-.is().not().oneOf(['Passw0rd', 'Password123']);
+  .is().min(8)
+  .has().uppercase()
+  .has().lowercase()
+  .has().digits()
+  .has().not().spaces()
+  .is().not().oneOf(['Passw0rd', 'Password123']);
 
 
 // Middleware that create a new user
 exports.signup = (req, res, next) => {
   if (!req.body.name || !req.body.email || !req.body.password) {
-    return res.status(400).json({ error: "Les champs doivent être remplis" });
-}
-if (emailValidator.validate(req.body.email)) {
-  if(passwordSchema.validate(req.body.password)) {
-      bcrypt.hash(req.body.password, 10)
-          .then(hash => {
-              const user = {
-                name: req.body.name,
-                email: req.body.email,
-                password: hash,
-              };
-              User.create(user)
-                  .then(user => {
-                    res.status(200).json({
-                      userId: user._id,
-                      token: jwt.sign(
-                        { userId: user._id },
-                        'RANDOM_TOKEN_SECRET',
-                        { expiresIn: '24h' }
-                      )
-                    });
-                  })
-                  .catch(error => res.status(400).json({ error }));
-              })
-          .catch(error => res.status(500).json({ error }));
-  } else {
-      res.status(401).json({ error: "Mot de passe requis : 8 caractères minimun, 1 Majuscule, 1 minuscule, sans espaces." });
+    return res.status(400).json({
+      error: "Les champs doivent être remplis"
+    });
   }
-} else {
-  res.status(401).json({ error: "Vérifier que votre email est valide" });
-}
+  if (emailValidator.validate(req.body.email)) {
+    if (passwordSchema.validate(req.body.password)) {
+      bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+          const user = {
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+          };
+          User.create(user)
+            .then(user => {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign({
+                    userId: user._id
+                  },
+                  'RANDOM_TOKEN_SECRET', {
+                    expiresIn: '24h'
+                  }
+                )
+              });
+            })
+            .catch(error => res.status(400).json({
+              error
+            }));
+        })
+        .catch(error => res.status(500).json({
+          error
+        }));
+    } else {
+      res.status(401).json({
+        error: "Mot de passe requis : 8 caractères minimun, 1 Majuscule, 1 minuscule, sans espaces."
+      });
+    }
+  } else {
+    res.status(401).json({
+      error: "Vérifier que votre email est valide"
+    });
+  }
 };
 // Middleware for user login 
 
 exports.login = (req, res, next) => {
   // check if field password is empty
   if (!req.body.password) {
-    return res.status(400).json({ error: "Les champs doivent être remplis" });
-}
+    return res.status(400).json({
+      error: "Les champs doivent être remplis"
+    });
+  }
   // check if user email is in database
   if (passwordSchema.validate(req.body.password)) {
-    User.findOne({ where: { email: req.body.email } })
-        .then(user => {
-            if (user) {
-                bcrypt.compare(req.body.password, user.password)
-                    .then(valid => {
-                        if (!valid) {
-                            res.status(401).json({ error: "Mot de passe incorrect !" });
-                        } else {
-                          res.status(200).json({
-                            userId: user._id,
-                            token: jwt.sign(
-                              { userId: user._id },
-                              'RANDOM_TOKEN_SECRET',
-                              { expiresIn: '24h' }
-                            )
-                          });
-                        }
-                    })
-                    .catch(error => res.status(500).json({ error }));
-            } else {
-                res.status(401).json({ error: "Connexion refusée" });
-            }
-        })
-        .catch(error => res.status(500).json({ error }))
-} else {
-    res.status(401).json({ error: "Mot de passe incorrect !" });
-}
+    User.findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      .then(user => {
+        if (user) {
+          bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+              console.log(valid)
+              console.log(user.password)
+              console.log(req.body.password)
+              if (!valid) {
+                res.status(401).json({
+                  error: "Mot de passe incorrect !"
+                });
+              } else {
+                res.status(200).json({
+                  userId: user._id,
+                  token: jwt.sign({
+                      userId: user._id
+                    },
+                    'RANDOM_TOKEN_SECRET', {
+                      expiresIn: '24h'
+                    }
+                  )
+                });
+              }
+            })
+            .catch(error => res.status(500).json({
+              error
+            }));
+        } else {
+          res.status(401).json({
+            error: "Connexion refusée"
+          });
+        }
+      })
+      .catch(error => res.status(500).json({
+        error
+      }))
+  } else {
+    res.status(401).json({
+      error: "Mot de passe incorrect !"
+    });
+  }
 };
 
 // Middleware to get user profil 
