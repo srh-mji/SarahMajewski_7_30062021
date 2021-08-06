330981c067a84882086eb810b9291d351d19d9b1
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-06 14:49:29 +0200

    Create view Account

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -1,17 +1,44 @@
 <template>
-  <div id="profil">
-    <Header/>
-      <h1>Modifier votre profil</h1>
+    <div id="account">
+    <Login v-if="!connected" />
+
+    <Header v-if="connected"/>
+    <UserAccount v-if="connected"/>
   </div>
 </template>
 
 <script>
+import Login from '@/components/Login.vue';
 import Header from '@/components/Header.vue';
+import UserAccount from '@/components/UserAccount.vue';
 
 export default({
     name : 'Account',
     components : {
-        Header
-    }
+        Login,
+        Header,
+        UserAccount,
+    },
+     data() {
+    return{
+      connected: true
+    };
+  },
+
+  created(){
+    this.checkConnected()
+  },
+
+   methods: {
+    checkConnected(){
+      if(localStorage.user !== undefined){
+        this.connected = true;
+        console.log('Utilisateur connecté !');
+      }
+      else if(localStorage.user == undefined){
+        this.connected = false;
+        console.log('Utilisateur non connecté !');
+      }
+    }},
 })
 </script>
