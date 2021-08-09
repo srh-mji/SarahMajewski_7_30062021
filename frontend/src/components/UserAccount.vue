6e02c67a715a2b1afb4bb6e4bff0ad760a2b6e11
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-09 14:33:32 +0200

    Add userAccount.vue

---
+++ frontend/src/components/UserAccount.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="UserAccount">
+    <h1>Modifier votre profil !</h1>
+    <a type="submit" @click = deleteAccount()> Supprimer le profil </a>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'UserAccount',
+
+    methods: {
+        deleteAccount(id){
+            axios.delete(`http://localhost:3000/api/user/account/${id}`,
+                    {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${this.$token}`
+                        }
+                    }
+                )
+                .then(
+                localStorage.removeItem('user'))
+                // .then(
+                // location.href = '/')
+
+                .catch((error) => {
+                    error
+                });
+            }
+    }
+}
+</script>
