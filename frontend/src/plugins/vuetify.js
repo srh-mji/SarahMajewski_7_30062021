085040cb915093af9902bc25227fb49e60055196
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-10 15:07:22 +0200

    Correct for use of vuetify

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,7 +1,10 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
 
-Vue.use(Vuetify);
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-export default new Vuetify({
-});
+Vue.use(Vuetify)
+
+const opts = {}
+
+export default new Vuetify(opts)
