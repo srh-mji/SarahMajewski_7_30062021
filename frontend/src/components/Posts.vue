5bdb514f7bc92619e12280ca7e8b6cd3b62fc0e1
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-12 13:07:06 +0200

    Display post in posts

--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -1,11 +1,43 @@
-<template>
- <div class="post">
-     <article>
-         <img src="post.User.image" alt="Image de profil">
-         <span> Posté le {{post.createAt}}</span>
-         <span> Par{{post.User.name}}</span>
-     </article>
-    </div>
+<template> 
+ <v-layout wrap>
+ <v-flex md4 v-for="post in posts" :key="post.id">
+  <v-card  
+    class="mx-auto"
+    color="#E4E4E4"
+    dark
+    max-width="400"
+  >
+    <v-card-title>
+      <v-avatar
+                  size="36px"
+                >
+                  <img v-if="post.User.image"
+                    alt="Avatar"
+                    src="post.User.image"
+                  >
+                  <v-icon dark v-else>
+                     mdi-account-circle
+                  </v-icon>
+                </v-avatar>
+
+         <span class="post-userName">Par {{post.User.name}}</span>
+         <span class="post-info"> Posté le {{post.createdAt}}</span>
+    </v-card-title>
+    <v-card-text>
+            <p>
+              {{post.message}}
+            </p>
+    </v-card-text>
+    <v-img
+            alt="image postée par l'utilisateur"
+            :max-height="600"
+            :max-width="400"
+            class="mx-auto pb-5"
+          >
+    </v-img>
+  </v-card>
+  </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -40,3 +72,7 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+</style>
