dec04389d47ce34e4836c41a22afb22f743c6d92
Sarah Majewski <sarah.majewski80@gmail.com>
2021-08-17 14:49:03 +0200

    Delete getUserPosts

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -18,7 +18,6 @@ router.get('/:id',auth, postCtrl.getOnePost);
 router.post('/',auth, multer, postCtrl.createOnePost);
 router.put('/:id',auth, multer, postCtrl.modifyOnePost);
 router.delete('/:id',auth, multer, postCtrl.deleteOnePost);
-router.get('/users/:id',auth, postCtrl.getUserPosts);
 
 // Comments
 router.post('/:id/comments',auth, multer, postCtrl.createOneComment);
