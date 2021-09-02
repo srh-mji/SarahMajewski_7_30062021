44e17251a63eccca10b2fe7353fe59f7966c9276
Sarah Majewski <sarah.majewski80@gmail.com>
2021-09-02 10:26:32 +0200

    Add post page for post edit

--- frontend/src/components/NewPost.vue
+++ frontend/src/components/NewPost.vue
@@ -3,6 +3,17 @@
         <form @submit.prevent="createOnePost()" >
             <label for="postContent">Contenu</label>
             <textarea name="message" v-model="message" id="message" cols="30" rows="10" placeholder="Quoi de neuf ?"></textarea>
+            <div>
+               <label for="image" class="pr-2">Image</label>
+               <input
+                type="file"
+                accept="image/png, image/jpeg,
+                image/bmp, image/gif"
+                ref="file"
+                name="Charger une image"
+              />
+              </div>
+            <!-- <button @click="uploadImage" type="submit"> Publier</button> -->
             <button type="submit"> Publier</button>
         </form>
     </div>
@@ -17,24 +28,32 @@ export default {
         return {
             post: [],
              message: "",
+             file: ""
         }
     },
 
     methods: {
+        uploadImage() {
+            const file = this.$refs.file.files[0];
+            this.file = file;
+        },
         createOnePost(){
-            axios.post(`http://localhost:3000/api/post/`,
-                {
-                        'message':this.message,
-                    },
+            this.uploadImage();
+            let DataForm = new FormData();
+            DataForm.append('message' , this.message);
+            DataForm.append('image' , this.file);
+
+            axios.post(`http://localhost:3000/api/post/`, DataForm,
                 {
                     headers: {
-                        'Content-Type': 'application/json',
+                        'Content-Type': 'multipart/form-data',
                         'Authorization': `Bearer ${this.$token}`
                     }
                 }
             )
             
-            .then(this.$root.$emit('Posts'));
+            .then(this.$root.$emit('Posts'))
+            .then(location.href = '/')
         },
     }
 }
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Signup from '../views/Signup.vue'
 import Account from '../views/Account.vue'
+import Post from '../views/Post.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/account',
     name: 'Account',
     component: Account
+  },
+  {
+    path: '/post',
+    name: 'Post',
+    component: Post
   },
 ]
 
