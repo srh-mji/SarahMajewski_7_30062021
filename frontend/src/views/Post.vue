fd4b7716ac03f4d7bde4762c395effe5a0429b24
Sarah Majewski <sarah.majewski80@gmail.com>
2021-09-02 10:32:35 +0200

    Add Post.Vue

--- frontend/src/views/Post.vue
+++ frontend/src/views/Post.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="post">
-    <EditPost/>
+    <Login v-if="!connected" />
+    <Header v-if="connected"/>
+    <EditPost v-if="connected"/>
   </div>
 </template>
 
 <script>
 import EditPost from '@/components/EditPost.vue';
+import Login from '@/components/Login.vue';
+import Header from '@/components/Header.vue';
 
 export default({
     name : 'Post',
     components : {
+        Login,
+        Header,
         EditPost
-    }
+    },
+    
+    data() {
+    return{
+      connected: true
+    };
+  },
+
+   created(){
+    this.checkConnected()
+    },
+    
+   methods: {
+    checkConnected(){
+      if(localStorage.user !== undefined){
+        this.connected = true;
+        console.log('Utilisateur connecté !');
+      }
+      else if(localStorage.user == undefined){
+        this.connected = false;
+        console.log('Utilisateur non connecté !');
+      }
+    }},
 })
 
 </script>
