15cb4542eb5bd7c550c462017f8fd3f0f41ad3ed
Sarah Majewski <sarah.majewski80@gmail.com>
2021-09-02 10:26:50 +0200

    add postVue

---
+++ frontend/src/views/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="post">
+    <EditPost/>
+  </div>
+</template>
+
+<script>
+import EditPost from '@/components/EditPost.vue';
+
+export default({
+    name : 'Post',
+    components : {
+        EditPost
+    }
+})
+
+</script>
