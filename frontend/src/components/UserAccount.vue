e9750b10d8a656456472891340b7ea58b3b2ffc1
Sarah Majewski <sarah.majewski80@gmail.com>
2021-09-18 14:42:30 +0200

    Add user card

--- frontend/src/components/UserAccount.vue
+++ frontend/src/components/UserAccount.vue
@@ -1,14 +1,45 @@
 <template>
 <div class="UserAccount">
     <h1>Modifier votre profil {{user.name}}!</h1>
+     <v-flex>
+  <v-card  
+    class="mx-auto"
+    color="#E4E4E4"
+    dark
+    max-width="400"
+  >
+    <v-card-title>
+      <v-avatar
+                  size="36px"
+                >
+                  <img v-if="user.image"
+                    alt="Avatar"
+                    :src="user.image"
+                  >
+                  <v-icon v-else dark>
+                     mdi-account-circle
+                  </v-icon>
+                </v-avatar>
+
+         <span class="post-userName">{{user.name}}</span>
+         <span class="post-info"> Création du compte le {{user.createdAt}}</span>
+    </v-card-title>
+    <v-card-text>
+            <p>
+              {{user.biography}}  
+            </p>
+    </v-card-text>
+  </v-card>
+  </v-flex>
+
     <form>
              <div class="form-group">
                 <label for="nom">Nom </label>
-                <input type="name" class="form-control" v-model="name" id="name" placeholder="Entrez votre nom" maxlength="50" required>
+                <input type="name" class="form-control" v-model="name" id="name" placeholder="Entrez votre nom" maxlength="50" required value="">
             </div>
             <div>
                 <label for="postContent">Biographie</label>
-                <textarea name="biography" v-model="biography" id="biography" cols="30" rows="10" placeholder="Rentrez ici votre biographie"></textarea>
+                <textarea name="biography" v-model="biography" id="biography" cols="30" rows="10" placeholder="Rentrez ici votre biographie" required></textarea>
             </div>
             <div>
                <label for="image" class="pr-2">Image de profil</label>
@@ -20,9 +51,9 @@
                 name="Charger une image"
               />
               </div>
-            <button type="submit" @click = updateAccount($user.userId)> Modifier votre profil </button>
+            <button type="submit" @click = updateAccount(user.id)> Modifier votre profil </button>
          </form>
-    <button type="submit" @click = deleteAccount()> Supprimer le profil </button>
+    <button type="submit" @click = deleteAccount(user.id)> Supprimer le profil </button>
 </div>
 </template>
 
@@ -39,6 +70,11 @@ export default {
             file: ""
         };
     },
+    mounted() {
+        if(localStorage.user != undefined){
+            this.getAccount();
+        }
+    },
 
     methods: {
         uploadImage() {
@@ -46,7 +82,7 @@ export default {
             this.file = file;
         },
         getAccount(){
-            const userId = this.userId;
+            const userId = JSON.parse(localStorage.getItem('user')).userId;
             axios.get(`http://localhost:3000/api/user/account/${userId}`,
             {
                         headers: {
@@ -57,10 +93,10 @@ export default {
             )
             .then(res => {
                 this.user = res.data;
+                console.log(this.user)
             })
         },
-        deleteAccount(){
-            const userId = this.userId;
+        deleteAccount(userId){
             axios.delete(`http://localhost:3000/api/user/account/${userId}`,
                     {
                         headers: {
@@ -95,7 +131,10 @@ export default {
             )
             
             .then(this.$root.$emit('Users'))
-            .then(location.reload())
+            // .then(location.reload())
+            .catch((error) => {
+                    error
+                });
         },
     }
 }
