2194a56daec48bc512f7647c86e277ff42f1e490
Sarah Majewski <sarah.majewski80@gmail.com>
2021-09-18 14:42:43 +0200

    Add member view

---
+++ frontend/src/views/Member.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="account">
+    <Login v-if="!connected" />
+
+    <Header v-if="connected"/>
+    <AllMember v-if="connected"/>
+  </div>
+</template>
+
+<script>
+import Login from '@/components/Login.vue';
+import Header from '@/components/Header.vue';
+import AllMember from '@/components/AllMember.vue';
+
+export default({
+    name : 'Account',
+    components : {
+        Login,
+        Header,
+        AllMember,
+    },
+     data() {
+    return{
+      connected: true
+    };
+  },
+
+  created(){
+    this.checkConnected()
+  },
+
+   methods: {
+    checkConnected(){
+      if(localStorage.user !== undefined){
+        this.connected = true;
+        console.log('Utilisateur connecté !');
+      }
+      else if(localStorage.user == undefined){
+        this.connected = false;
+        console.log('Utilisateur non connecté !');
+      }
+    }},
+})
+</script>
